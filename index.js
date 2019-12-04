4647653c52a81149f63aa031486369b5220aaf5f
Vinícius Rodrigues <viniciusrdefreitas@gmail.com>
2019-12-03 22:48:03 -0300

    Conexão com SQL e alteração das views junto com a index.js

--- index.js
+++ index.js
@@ -1,16 +1,53 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const connection = require('./database/database');
+const Pergunta = require('./database/Pergunta');
+
+connection
+  .authenticate()
+  .then(() => {
+    console.log('Conexão feita com o banco de dados!');
+  })
+  .catch((msgErro) => {
+    console.log(msgErro);
+  });
+
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 
 app.get('/', (req, res) => {
-  res.render('index')
-})
+  Pergunta.findAll({ raw: true }).then(perguntas => {
+    res.render('index', {
+      perguntas: perguntas
+    });
+  });
+});
 
 app.get('/perguntar', (req, res) => {
   res.render('perguntar');
+});
+
+app.post("/salvarPergunta", (req, res) =>{
+
+  var titulo = req.body.titulo;
+  var descricao = req.body.descricao;
+
+  if(titulo == null || descricao == null){
+    res.redirect('/');
+  }
+
+  Pergunta.create({
+    titulo: titulo,
+    descricao: descricao
+  }).then(() => {
+    res.redirect('/');
+  });
 });
 
 app.listen(4000, (erro) => {
